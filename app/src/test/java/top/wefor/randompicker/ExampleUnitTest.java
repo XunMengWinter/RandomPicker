c8b41c1d71e6a81cd934694c9a4fafa0dec1bf2d
Winter <XunMengWinter@gmail.com>
2018-08-14 04:14:32 +0800

    重置翻牌模式测试代码。

--- app/src/test/java/top/wefor/randompicker/ExampleUnitTest.java
+++ app/src/test/java/top/wefor/randompicker/ExampleUnitTest.java
@@ -2,6 +2,11 @@ package top.wefor.randompicker;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+
 /**
  * To work on unit tests, switch the Test Artifact in the Build Variants view.
  */
@@ -21,7 +26,46 @@ public class ExampleUnitTest {
 //            System.out.println(times);
 //        }
 
-        randomTest();
+//        randomTest();
+//        for (int i=1;i<20;i++){
+//            System.out.print(i);
+        pokerTest(9);
+//        }
+    }
+
+    /**
+     * 重置翻牌随机。
+     * 综合概率为 2/(n+1)。
+     */
+    private void pokerTest(int num) {
+        List<Integer> list = new ArrayList<>();
+        for (int x = 0; x < num; x++)
+            list.add(x);
+        Collections.shuffle(list);
+
+        Random random = new Random();
+        int luckyCount = 0;
+        int lastNum = -1;
+        int comboCount = 0;
+        for (int i = 0; i < 10_000; i++) {
+            int index = random.nextInt(list.size());
+            int lucky = list.remove(index);
+            if (lucky == num / 2) { //取中间值作为中奖数
+                if (lastNum == lucky) {
+                    //连中
+                    comboCount++;
+                }
+                luckyCount++;
+                //抽中后重新洗牌
+                list.clear();
+                for (int x = 0; x < num; x++)
+                    list.add(x);
+                Collections.shuffle(list);
+            }
+            lastNum = lucky;
+        }
+        System.out.println("10,000 times, comboCount: " + comboCount);
+        System.out.println("pokers = " + num + "  , probability = " + (luckyCount / 10_000f));
     }
 
     private void randomTest() {
@@ -29,7 +73,7 @@ public class ExampleUnitTest {
         final RandomPicker randomPicker = new RandomPicker();
         randomPicker.setCalculator(new Calculator() {
             @Override
-            public int calculateNextWeight(int currentWeight, int originWeight) {
+            public int calculateNextWeight(int position, int currentWeight, int originWeight) {
                 if (currentWeight == 0) {
                     //加大连击概率
                     return size;
